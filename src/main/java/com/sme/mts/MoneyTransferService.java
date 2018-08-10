a7f706d7449702ec20615e3c48f787b48f524299
hxh_robb <hxh.robb@gmail.com>
2018-08-10 11:39:41 +0800

    Add charsetEncoding to JDBC url

--- src/main/java/com/sme/mts/MoneyTransferService.java
+++ src/main/java/com/sme/mts/MoneyTransferService.java
@@ -12,13 +12,33 @@ public class MoneyTransferService
 	}
 
 //    private static final org.apache.commons.logging.Log logger = org.apache.commons.logging.LogFactory.getLog(MoneyTransferService.class);
-//	private static final String KEY = "robb.test";
-//    @org.springframework.beans.factory.annotation.Value("${" + KEY + "}")
-//	private String value;
+//    private static final String [] ENV_VARS = {
+//        "JDBC_URL","JDBC_USER","JDBC_PASS",
+//    };
+//    private static final String [] PROP = {
+//        "spring.datasource.url",
+//        "spring.datasource.username",
+//        "spring.datasource.password",
+//    };
 //
+//    @org.springframework.beans.factory.annotation.Autowired
+//    private org.springframework.core.env.Environment env;
 //
 //    @Override
 //    public void run(String... args) throws Exception {
-//        logger.info(KEY + ":" + value);
+//        for (String key: ENV_VARS) {
+//            logger.info("[ENV]" + key + ":" + System.getenv(key));
+//        }
+//
+//        for (String key: PROP) {
+//            logger.info("[PROP]" + key + ":" + env.getProperty(key));
+//        }
+//
+//        try {
+//            java.net.InetAddress addr = java.net.InetAddress.getByName("mariadb");
+//            logger.info("IP(\"mariadb\"):" + addr.getHostAddress());
+//        } catch (Throwable th) {
+//            logger.info("IP(\"mariadb\") not found");
+//        }
 //    }
 }
