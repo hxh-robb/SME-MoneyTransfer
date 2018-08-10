482ec3a116979362fe447e3e4f71b9a2342540b8
hxh_robb <hxh.robb@gmail.com>
2018-08-10 09:19:22 +0800

    Add support for env-var jdbc setting

--- src/main/java/com/sme/mts/MoneyTransferService.java
+++ src/main/java/com/sme/mts/MoneyTransferService.java
@@ -4,18 +4,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MoneyTransferService /*implements CommandLineRunner*/ {
-//    private static final Log logger = LogFactory.getLog(MoneyTransferService.class);
+public class MoneyTransferService
+//    implements org.springframework.boot.CommandLineRunner
+{
 	public static void main(String[] args) {
 		SpringApplication.run(MoneyTransferService.class, args);
 	}
 
-//	@Value("${spring.datasource.url}")
-//	String uri;
+//    private static final org.apache.commons.logging.Log logger = org.apache.commons.logging.LogFactory.getLog(MoneyTransferService.class);
+//	private static final String KEY = "robb.test";
+//    @org.springframework.beans.factory.annotation.Value("${" + KEY + "}")
+//	private String value;
 //
 //
 //    @Override
 //    public void run(String... args) throws Exception {
-//        logger.info(uri);
+//        logger.info(KEY + ":" + value);
 //    }
 }
